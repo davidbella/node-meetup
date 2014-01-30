c451ed497b6168d9b80de2a0fc532248d0e713dd
David Bella <dbella1@gmail.com>
2014-01-30 18:36:37 -0500

    Finally, node pubsub is working

--- Project/server.js
+++ Project/server.js
@@ -4,16 +4,32 @@ var express = require('express'),
 var path = require('path');
 var app = express();
 
+var faye = require('faye');
+var http = require('http');
+
+var bayeux = new faye.NodeAdapter({mount: '/faye', timeout: 45});
+var server = http.createServer();
+bayeux.attach(server);
+server.listen(3001);
+
+function sendUpdateMessage() {
+  bayeux.getClient().publish('/faye', {text: 'message'});
+};
+
 app.use(express.bodyParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', function (req, res) {
-    res.end('Hello Node!');
+  res.end('Hello Node!');
 });
 
 app.get('/items', items.findAll);
 app.get('/items/:id', items.findById);
-app.post('/items', items.addItem);
+app.post('/items', function(req, res) {
+  sendUpdateMessage();
+  console.log("here");
+  items.addItem(req, res);
+});
 app.put('/items/:id', items.updateItem);
 app.delete('/items/:id', items.deleteItem);
 
